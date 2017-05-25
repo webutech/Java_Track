a1c4a4dfc991abd47e20cacc118bc860521f0164
webutech <pankaj.wedutech@gmail.com>
2017-05-25 22:31:50 +0530

    Spring Boot Todo Basic Set up

---
+++ Spring Boot/springboot-todo-basic/src/main/java/com/yash/springboot/startup/TodoStartupApplication.java
@@ -0,0 +1,12 @@
+package com.yash.springboot.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoStartupApplication.class, args);
+	}
+}
