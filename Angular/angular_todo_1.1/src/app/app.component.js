a2d461fdb1c2bcd48c150b85f4d1019006889284
webutech <pankaj.wedutech@gmail.com>
2017-05-25 12:56:36 +0530

    Todo _ Master_Detail and Component added

---
+++ Angular/angular_todo_1.1/src/app/app.component.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var TODOS = [
+    { id: 1, name: 'Create Angular2 todo app' },
+    { id: 2, name: 'Take Rest WS Test' },
+    { id: 3, name: 'Create PPT for Angular todo' },
+    { id: 4, name: 'Check mails' },
+    { id: 5, name: 'Take Interview' },
+    { id: 6, name: 'Talk to Rakeshji' },
+    { id: 7, name: 'Prepare Questions for Interview' },
+    { id: 8, name: 'Prepare Test' },
+    { id: 9, name: 'Talk to narendra sir' },
+    { id: 10, name: 'Plan for team lunch' }
+];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Todo - App';
+        this.todos = TODOS;
+    }
+    AppComponent.prototype.onSelect = function (todo) {
+        this.selectedTodo = todo;
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n  <h1>{{title}}</h1>\n  <h2>My Todos</h2>\n  <ul class=\"todos\">\n    <li *ngFor=\"let todo of todos\" [class.selected]=\"todo === selectedTodo\" (click)=\"onSelect(todo)\">\n      <span class=\"badge\">{{todo.id}}</span>{{todo.name}}\n    </li>\n  </ul>\n  <todo-detail [todo]=\"selectedTodo\"></todo-detail> \n  ",
+        styles: ["\n\n  .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .todos{\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n\n    .todos li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n\n  .todos li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n\n   .todos li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .todos .text {\n    position: relative;\n    top: -3px;\n  }\n  .todos .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n  \n  "]
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ Angular/angular_todo_1.1/src/app/app.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+import { Todo } from './todo';
+
+const TODOS : Todo[]=[
+  { id:1, name : 'Create Angular2 todo app'},
+  { id:2, name : 'Take Rest WS Test'},
+  { id:3, name : 'Create PPT for Angular todo'},
+  { id:4, name : 'Check mails'},
+  { id:5, name : 'Take Interview'},
+  { id:6, name : 'Talk to Rakeshji'},
+  { id:7, name : 'Prepare Questions for Interview'},
+  { id:8, name : 'Prepare Test'},
+  { id:9, name : 'Talk to narendra sir'},
+  { id:10, name : 'Plan for team lunch'}
+];
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My Todos</h2>
+  <ul class="todos">
+    <li *ngFor="let todo of todos" [class.selected]="todo === selectedTodo" (click)="onSelect(todo)">
+      <span class="badge">{{todo.id}}</span>{{todo.name}}
+    </li>
+  </ul>
+  <todo-detail [todo]="selectedTodo"></todo-detail> 
+  `,
+  styles : [`
+
+  .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .todos{
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+
+    .todos li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+
+  .todos li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+
+   .todos li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .todos .text {
+    position: relative;
+    top: -3px;
+  }
+  .todos .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+  
+  `]
+})
+export class AppComponent  {
+ title='Todo - App';
+ todos=TODOS;
+ selectedTodo:Todo;
+ onSelect(todo: Todo):void {
+   this.selectedTodo=todo;
+ }
+
+}
